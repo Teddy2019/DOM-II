c10be368ee76909ff12ae4a337ef518fbbbb8b9e
Your Name <tfikrie@yahoo.com>
2019-05-02 23:41:23 -0400

    nav changed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const nav = document.querySelector('nav');
+nav.addEventListener('mouseover', function(e){e.target.style.color = "orange";
+e.target.style.padding = "4px";
+ e.target.style.background = "black";
+setTimeout(function() {e.target.style.color = ""; e.target.style.background = ""}, 500);}, false);
+console.log(nav);
